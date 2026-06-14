4bfcb4a74b377f4c338cbfe03c558b803feb588d
fleischm-hue <fleischm@mac.com>
2026-06-14 16:03:00 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="campsite-dogs-tables-close-v8";
-const FILES=["./","./index.html","./manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+// v10 intentionally does not cache files while testing.
+self.addEventListener("install", e => self.skipWaiting());
+self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", e => e.respondWith(fetch(e.request)));
